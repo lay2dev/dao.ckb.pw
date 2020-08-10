d387f668feb880d52598ef810df8081f446f89dd
zhixian <zhixian@yamen.co>
2020-08-10 13:57:06 +0800

    add build config

---
+++ build_prod.sh
@@ -0,0 +1,1 @@
+PUBLIC_PATH=https://pw-dao.oss-cn-beijing.aliyuncs.com/static/v0.1.0/ quasar b -m pwa
--- quasar.conf.js
+++ quasar.conf.js
@@ -43,6 +43,7 @@ module.exports = function(/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "hash", // available values: 'hash', 'history'
+      publicPath: process.env.PUBLIC_PATH,
 
       // Add dependencies for transpiling with Babel (Array of regexes)
       // (from node_modules, which are by default not transpiled).
